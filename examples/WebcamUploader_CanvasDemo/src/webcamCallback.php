2a4c9d8b1103ed50d5c1ad29347ad3d0cbc343e7
wgorgichuk <wgorgichuk@046cc19f-f133-0410-bb18-b3d48ac8bcd4>
2009-03-30 21:44:56 +0000

    Added example code.

---
+++ examples/WebcamUploader_CanvasDemo/src/webcamCallback.php
@@ -0,0 +1,21 @@
+<?
+
+//print_r($_POST);
+//$sessionKey = $_POST['fb_sig_session_key'];
+
+?>
+
+<fb:add-section-button section="profile"/>
+<hr />
+<!-- 
+Absolute URL to the SWF on your server. 
+
+-->
+<fb:swf id='facebookApp'
+		swfsrc='webCamUploader.swf'
+		imgsrc='webcampreview.jpg'
+		swfbgcolor='006599'
+		width='214' 
+		height='255' /> 
+
+
